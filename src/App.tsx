0fde774fed9a80f0ece85a90ba0f28aefea9a912
RomanZvol <roman.zvol@gmail.com>
2026-02-26 22:34:39 +0200

    Adding basic structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      flexDirection: 'column',
+      fontFamily: 'system-ui, -apple-system, sans-serif'
+    }}>
+      <h1>Hello World</h1>
+      <p>OCR App project started</p>
+    </div>
+  )
+}
+
+export default App
